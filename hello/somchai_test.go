c8d5f75739e2da2bafba9c8580bca9e0dbfe3452
endkarn <end_karn@hotmail.com>
2019-06-25 16:32:15 +0700

    Update function

--- hello/somchai_test.go
+++ hello/somchai_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestGoodAfternoonToSomchai(t *testing.T) {
-	expected := "Good Afternoon. Somchai."
+	expected := "Very Good Afternoon. Somchai."
 	actual := Greeting()
 	if expected != actual {
 		t.Errorf("Expected %q but %q", expected, actual)
